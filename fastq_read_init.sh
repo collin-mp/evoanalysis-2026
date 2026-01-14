f91cca08f1ac59be035d67aaba179ab27a9dad03
collin-mp <cporte26@uwyo.edu>
2026-01-14 14:40:30 -0700

    initial FASTQ read commands

---
+++ fastq_read_init.sh
@@ -0,0 +1,7 @@
+## initial commands for fastq read scripts
+cd /project/evoanalysis/d3challenge_fq
+
+## create list for sequences, count # for array tasks
+ls *.fastq > ../cporte26/evoanalysis-2026/fastq_list.txt
+
+cd ../cporte26/evoanalysis-2026
